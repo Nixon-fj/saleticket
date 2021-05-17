0dc7a3dd6c676d2ca7bba9ded901ffe6cb289fe3
Nixon-fj <71091247+Nixon-fj@users.noreply.github.com>
2021-05-17 15:56:30 -0600

    cargado
    cargado del proyecto

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//funciones del menu
+function openMenu(){
+    let menuMob=document.body.getElementsByClassName('nav')[0];
+    menuMob.style.left="0";
+}
+function closeMenu(){
+    let menuMob=document.body.getElementsByClassName('nav')[0];
+    menuMob.style.left="-35em";
+}
+//funciones del slider
